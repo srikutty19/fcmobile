d29975da721907f521d8fb271d6d20e8ba07b2a6
table <sri9132@gmail.com>
2019-02-15 17:51:15 +0530

    initial commit

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+var krms_config ={			
+	'ApiUrl':"http://foodieschoice.tk/mobileapp/api",	
+	'DialogDefaultTitle':"KMRS",	
+	'pushNotificationSenderid':"",	
+	'facebookAppId':"", 
+	'APIHasKey':"2b0926cd0759663bca9051fca11a619e"
+};
